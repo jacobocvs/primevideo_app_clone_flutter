0b7683a048603f3ea7353185bc0a67c907a0fcc4
Jacobo <al.jacvs@gmail.com>
2022-04-18 23:43:52 -0500

    SilverAppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,51 @@
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'UI of Amazon Prime Video',
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  @override
-  _MyAppState createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
 
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        backgroundColor: Color (0xFF1b1b1e),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            floating: true,
+            expandedHeight: 130.0,
+          backgroundColor: Color (0xFF1b1b1e),
+            flexibleSpace: FlexibleSpaceBar(
+              background: FlutterLogo()
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+      ),
     );
   }
 }
